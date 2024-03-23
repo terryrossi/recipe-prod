585e26cd41f88eae93f10a308c2d89bf6eccdb90
Terry-Rossi <terryrossi1@gmail.com>
2024-03-23 11:05:54 -0700

    changed allowed host in settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['secret-sea-18830.herokuapp.com']
+ALLOWED_HOSTS = ['https://secret-sea-18830-21168ebbdec3.herokuapp.com/']
 
 
 # Application definition
